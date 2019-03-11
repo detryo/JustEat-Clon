83223346bb79e7e8ef5813299a4f5c051c1223d6
Cristian <chris.sedano.arenas@gmail.com>
2019-03-11 16:57:13 +0000

    Interface Done
    Implement Navigation Controller, two TableView Controller and ViewController with imageView and four labels and config de views

---
+++ JustEat/Controllers/OrderViewController.swift
@@ -0,0 +1,22 @@
+//
+//  OrderViewController.swift
+//  JustEat
+//
+//  Created by Cristian Sedano Arenas on 6/3/19.
+//  Copyright © 2019 Cristian Sedano Arenas. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var orderLabel: UILabel!
+    @IBOutlet weak var costLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
